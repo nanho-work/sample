fe9b162320e68bce77d5a4634a67e9879ad96a62
nhchoi2 <choi1278@gmail.com>
2025-07-29 13:39:51 +0900

    1

--- js/speech.js
+++ js/speech.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+  console.log("window.onload: 음성 안내 시작");
   const msg = new SpeechSynthesisUtterance();
   msg.text = "안녕하세요. 한국도로공사 민원 포털입니다. 원하시는 서비스를 선택해주세요.";
   msg.lang = "ko-KR";
@@ -6,6 +7,7 @@ window.onload = () => {
 };
 
 function speakText(text) {
+  console.log("speakText 호출됨:", text);
   const msg = new SpeechSynthesisUtterance();
   msg.text = text;
   msg.lang = "ko-KR";
@@ -13,8 +15,10 @@ function speakText(text) {
 }
 
 function navigateAndSpeak(url, text) {
+  console.log("navigateAndSpeak 호출됨: URL =", url, "TEXT =", text);
   speakText(text);
   setTimeout(() => {
+    console.log("2초 후 이동 시작:", url);
     window.location.href = url;
   }, 2000); // 음성 안내 후 2초 뒤 이동
 }
